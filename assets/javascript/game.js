2d305a7d3088d389a0713a6d7e039c70c71d7a16
vin <vrp908@gmail.com>
2017-08-11 15:19:37 -0400

    second delivery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,41 +4,57 @@ $(document).ready(function() {
     var wordList = ["goat","dog"];
     var wordChoice = "";
     var guesses = 9;
-    var guessLog = "";
     var wordChoice = wordList[Math.floor(Math.random() * wordList.length)];
 
 
     var wordLetters     = [];
     var guessedLetters  = [];
+    var wrongLetters    = [];
+    var restart = false
 
     // take the word to be solved and split it into individual letters
     // use an array with blanks same length as chosen word
 
     for (var i =0; i < wordChoice.length; i++) {
-        wordLetters = wordLetters + wordChoice[i]
-        wordLetters = wordLetters + ","
-        console.log(wordLetters)
-        guessedLetters = guessedLetters + "_" + ","
-        console.log(guessedLetters)
+        wordLetters = wordLetters + wordChoice[i];
+         console.log(wordLetters);
+          guessedLetters[i] = '_';
+           console.log(guessedLetters);
     }
 
 
     document.onkeyup = function (event) {
 
-        var letter = event.key
-        var goodGuess = false
-        var moreToGuess = false
 
+        var letter = event.key;
+         var goodGuess = false;
+          var moreToGuess = false;
+           
+
+        if (restart) {
+            alert("game has restarted and new word has been chosen");
+            
+            wordChoice = wordList[Math.floor(Math.random() * wordList.length)];
+
+            for (var i =0; i < wordChoice.length; i++) {
+              wordLetters = wordLetters + wordChoice[i]
+               console.log(wordLetters)
+                guessedLetters[i] = '_'
+                 console.log(guessedLetters)
+            }
+            restart = false 
+            guesses = 9
+        }
+        
         if (guesses > 0) {
 
             for (var i = 0; i < wordLetters.length; i++) {
 
                if (wordLetters[i] == letter) {
-                  console.log(letter)
                   guessedLetters[i] = letter;
-                  console.log(guessedLetters)
                   goodGuess = true;
                }
+               
                if (guessedLetters[i] == '_') {
                    moreToGuess = true ;
                }
@@ -47,50 +63,58 @@ $(document).ready(function() {
 
             if (goodGuess) {
                 alert("you found a letter");
-                console.log(guessedLetters)
+                console.log(guessedLetters);
+                
+
                 if (!moreToGuess) {
-                    alert("you won!")
+                    restart = true
+                    guesses = 0
+                    alert("you won!");
                 }
+
             } 
 
             else {
+
                 alert("thats not right!");
+                console.log("letter" +letter)
+                console.log("before" + wrongLetters)
+                wrongLetters = wrongLetters + letter + ',';
+                console.log("wrong" +wrongLetters);
+                guesses--
             }  
+
+
+             var stats =
+            "<p>word status: " + guessedLetters + "</p>" +
+            "<p>already guessed " + wrongLetters + "</p>" +
+            "<p>guesses left: " + guesses + "</p>";
+            console.log(stats)
+
+
+            $("#game").html(stats);
+        }
+
+        else {
+
+        if (!restart) {
+      
+        restart = true
+         wrongLetters = [];
+          guesses = 9;
+
+        var stats =
+            "<p>"+ "you lost!!!!" + "</p>" +
+            "<p>already guessed " + wrongLetters + "</p>" +
+            "<p>guesses left: " + guesses + "</p>";
+            console.log(stats)
+
+        $("#game").html(stats);  
+
+        }
         }
      
     }
-    
-    // function guessLetter(letter) {
-    //    var goodGuess = false;
-    //    var moreToGuess = false;
-    //    for (var i = 0; i < wordLetters.length; i++) {
-    //        if (wordLetters[i] == letter) {
-    //           guessedLetters[i] = letter;
-    //            goodGuess = true;
-    //        }
-   //         if (guessedLetters[i] == '_') {
-   //             moreToGuess = true;
-   //         }
-   //     }
-   //     if (goodGuess) {
-   //         console.log('Yay, you found a letter!');
-   //         console.log(guessedLetters.join(''));
-   //         if (!moreToGuess) {
-   //             console.log('YOU WON!');
-   //         } 
-   //     } else {
-   //         console.log('Oh noes, thats not right!');
-   //     }
-  //  }
-//
-  //  document.onkeyup = function (event) {    
-  //      
-   //     var userLetter = event.key
-    //    for (var guesses = 9; guesses < 0; guesses--) {
-    //      guessLetter(userLetter[guesses]);
-     ///   } 
-//
-       
-    // This function is run whenever the user presses a key.
+
 
   });
